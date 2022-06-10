da133304aab37d0fe8eed7a3ed86d0285db28c41
Floki <ypr_tylerkeuler@aol.com>
2022-06-10 09:49:30 -0400

    sudosudosasuudioo

--- develop/script.js
+++ develop/script.js
@@ -1,1 +1,29 @@
-console.log("im working")
+
+
+
+
+
+
+
+
+
+
+// global vars
+
+
+// fetch weather api 
+
+
+// proccess the data from the weather api 
+
+//function to display previous searched items to a list. 
+
+// function to store data to local storage
+
+// 
+
+// dynamically update the page with the information from the api 
+
+// init function. 
+
+//
