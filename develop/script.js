d21f1cdd761f305c7dacfc9314e40319d228e9f2
Floki <ypr_tylerkeuler@aol.com>
2022-06-15 11:10:34 -0400

    finally figured out how to get the API call to work properly, now on to sorthing through the data to get the weather to populate to the page

--- develop/script.js
+++ develop/script.js
@@ -1,3 +1,23 @@
+var apiKey = 'c7dc3dc5930fbf4151b143bc06889fef'
+var cityName = document.querySelector('.weather-search')
+var searchButton = document.getElementById('search-button')
+var form = document.getElementById('search-form')
+var today = document.querySelector('#today')
+var forecast = document.querySelector('#forecast')
+
+searchButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + cityName.value + '&appid=' + apiKey+ '&units=imperial')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            today.innerHTML = data['list'][0]['dt_txt'];
+            forecast.innerHTML = data['list'][0]['main']['temp'];
+
+        })
+        .catch(err => alert("wrong city name!"))
+
+})
 
 
 
@@ -6,6 +26,39 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getApi() {
+//     console.log(cityName);
+//     console.log(requestUrl)
+//     fetch(requestUrl)
+//         .then(function (response) {
+//             return response.json()
+//         })
+//         .then(function (data) {
+//             console.log(data)
+
+//         });
+// };
+
+// searchButton.addEventListener('click', function(e){
+//     e.preventDefault();
+//     getApi();
+// });
 
 
 // global vars
@@ -24,6 +77,3 @@
 
 // dynamically update the page with the information from the api 
 
-// init function. 
-
-//
