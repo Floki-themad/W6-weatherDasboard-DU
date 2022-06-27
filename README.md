b7177e5bf244efb282d010769eceff131c34cb81
Floki <ypr_tylerkeuler@aol.com>
2022-06-27 16:01:54 -0400

    finally got the bug sorted out, was using a reserved word as a variable.  updated the read me and final push

--- README.md
+++ README.md
@@ -16,8 +16,21 @@ SO THAT I can plan a trip accordingly
 * THEN the user can enter another city name if they so choose to 
 * THEN the page will be re-populated based on the city searched. 
 
+## Instalation.
+* In the project repo copy the ```git clone``` URL
+* navigate to the folder you would like to place the repo in and open a terminal
+* in the terminal run the command ```git clone``` followed by the copied URL
+* now you have a copy of the repo on your local machine. 
 
-## Built with: HTML, CSS, JavaScript, Openweather API 
-### [Live Website](https://floki-themad.github.io/W6-weatherDasboard-DU/)
-<img src ="img/Screen Shot 2022-06-15 .png">
-<img src ="img/Screen Shot 2022-06-15 at 6.14.03 PM.png">
+
+## Built with: HTML, CSS, Bootstrap,  JavaScript, Openweather API, Jquery and Moment.js
+
+
+### Check out the [weather dashboard](https://floki-themad.github.io/W6-weatherDasboard-DU/) in action! 
+
+
+<img src ="img/Screen Shot 2022-06-27 at 3.48.15 PM.png">
+<img src ="img/Screen Shot 2022-06-27 at 3.50.39 PM.png">
+
+## Questions
+* Please feel free to email me with any questions! 
--- script.js
+++ script.js
@@ -1,115 +1,115 @@
-// var apiKey = 'c7dc3dc5930fbf4151b143bc06889fef'
-// let today = moment();
-// var location = document.getElementById("city-name");
-// const searchLocation = document.getElementById("search-city");
-// const cityArr = [];
+var apiKey = 'c7dc3dc5930fbf4151b143bc06889fef'
+let today = moment();
+ let locationName = document.getElementById("city-name");
+const searchLocation = document.getElementById("search-city");
+const cityArr = [];
 
-// // get api data on city 
-// function getData(inputCity){
-//     const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${inputCity}&limit=1&appid=${apiKey}`;
-//     fetch(apiUrl)
-//     .then(function(response){
-//         if(response.ok){
-//             response.json().then(function(data){
-//                 console.log(data);
-//                 if(data.length > 0){
-//                     const foundLocation = data[0].name;
-//                     if(foundLocation.toLowerCase() === inputCity.toLowerCase()){
-//                         const lat = data[0].lat;
-//                         const lon = data[0].lon;
-//                         const newApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${myApiKey}&units=imperial`;
+// get api data on city 
+function getData(inputCity){
+    const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${inputCity}&limit=1&appid=${apiKey}`;
+    fetch(apiUrl)
+    .then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                console.log(data);
+                if(data.length > 0){
+                    const foundLocation = data[0].name;
+                    if(foundLocation.toLowerCase() === inputCity.toLowerCase()){
+                        const lat = data[0].lat;
+                        const lon = data[0].lon;
+                        const newApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
 
-//                         fetch(newApiUrl)
-//                         .then(function(response){
-//                             return response.json();
-//                         })
-//                         .then(function(data){
-//                             console.log(data);
-//                             $("#current-city").html(foundLocation);
-//                             $("#current-date").html("(Today: " + today.format("l") + ")");
-//                             $("#current-icon").attr("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2.png");
-//                             $("#current-temp").html(data.current.temp + "F");
-//                             $("#current-wind").html(data.current.wind_speed + "mph")
-//                             $("#current-humidity").html(data.current.humidity + "%");
-//                             $("#current-index").html(data.current.uvi);
-//                             uvColor(data.current.uvi);
+                        fetch(newApiUrl)
+                        .then(function(response){
+                            return response.json();
+                        })
+                        .then(function(data){
+                            console.log(data);
+                            $("#current-city").html(foundLocation);
+                            $("#current-date").html("(Today: " + today.format("l") + ")");
+                            $("#current-icon").attr("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2.png");
+                            $("#current-temp").html(data.current.temp + "F");
+                            $("#current-wind").html(data.current.wind_speed + "mph")
+                            $("#current-humidity").html(data.current.humidity + "%");
+                            $("#current-index").html(data.current.uvi);
+                            uvColor(data.current.uvi);
 
-//                             for(let index = 1; index <= 5; index++){
-//                                 let dayId = "";
-//                                 dayId = "#day" + i + "-date";
-//                                 $(dayId).html(today.add(1, 'd').format("l"));
-//                                 dayId = "#day" + i + "-icon";
-//                                 $(dayId).attr("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png");
-//                                 dayId = "#day" + i + "-temp";
-//                                 $(dayId).html(data.daily[i].temp.max + "°F");
-//                                 dayId = "#day" + i + "-wind";
-//                                 $(dayId).html(data.daily[i].wind_speed + " mph");
-//                                 dayId = "#day" + i + "-humidity";
-//                                 $(dayId).html(data.daily[i].humidity + " %");
-//                               }
-//                               today = moment();
-//                               if(!cityArr.includes(foundLocation)){
-//                                   const newButton = document.createElement('button');
-//                                   newButton.classList.add("form-control");
-//                                   newButton.classList.add("btn");
-//                                   newButton.classList.add("btn-secondary");
-//                                   newButton.classList.add("add-space");
-//                                   newButton.textContent = foundLocation;
-//                                   searchLocation.appendChild(newButton);
-//                                   cityArr.push(foundLocation);
-//                                   localStorage.setItem("location", JSON.stringify(cityArr));
-//                               }
-//                         });
-//                     }else{
-//                         alert("That location not on this Planet!")
-//                     }
-//                 }
+                            for(let index = 1; index <= 5; index++){
+                                let dayId = "";
+                                dayId = "#day" + index + "-date";
+                                $(dayId).html(today.add(1, 'd').format("l"));
+                                dayId = "#day" + index + "-icon";
+                                $(dayId).attr("src", "http://openweathermap.org/img/wn/" + data.daily[index].weather[0].icon + "@2x.png");
+                                dayId = "#day" + index + "-temp";
+                                $(dayId).html(data.daily[index].temp.max + "°F");
+                                dayId = "#day" + index + "-wind";
+                                $(dayId).html(data.daily[index].wind_speed + " mph");
+                                dayId = "#day" + index + "-humidity";
+                                $(dayId).html(data.daily[index].humidity + " %");
+                              }
+                              today = moment();
+                              if(!cityArr.includes(foundLocation)){
+                                  const newButton = document.createElement('button');
+                                  newButton.classList.add("form-control");
+                                  newButton.classList.add("btn");
+                                  newButton.classList.add("btn-secondary");
+                                  newButton.classList.add("add-space");
+                                  newButton.textContent = foundLocation;
+                                  searchLocation.appendChild(newButton);
+                                  cityArr.push(foundLocation);
+                                  localStorage.setItem("location", JSON.stringify(cityArr));
+                              }
+                        });
+                    }else{
+                        alert("That location not on this Planet!")
+                    }
+                }
                 
-//             });
-//         }
-//     });
-// }
+            });
+        }
+    });
+}
 
-// function uvColor(uvIndex){
-//     if (uvIndex <= 2){
-//         $("#current-index").attr("style", "background-color: green;");
-//     }
-//     if((uvIndex > 2) && (uvIndex <= 5)){
-//         $("#current-index").attr("style", "background-color: yellow;");
-//     }
-//     if((uvIndex > 5) && (uvIndex <= 7)){
-//         $("#current-index").attr("style", "background-color: orange;");
-//     }
-//     if((uvIndex > 7) && (uvIndex <= 10)){
-//         $("#current-index").attr("style", "background-color: red;");
-//     }
-//     if((uvIndex > 10)){
-//         $("#current-index").attr("style", "background-color: purple;");
-//     }
-// }
+function uvColor(uvIndex){
+    if (uvIndex <= 2){
+        $("#current-index").attr("style", "background-color: green;");
+    }
+    if((uvIndex > 2) && (uvIndex <= 5)){
+        $("#current-index").attr("style", "background-color: yellow;");
+    }
+    if((uvIndex > 5) && (uvIndex <= 7)){
+        $("#current-index").attr("style", "background-color: orange;");
+    }
+    if((uvIndex > 7) && (uvIndex <= 10)){
+        $("#current-index").attr("style", "background-color: red;");
+    }
+    if((uvIndex > 10)){
+        $("#current-index").attr("style", "background-color: purple;");
+    }
+}
 
-// function init(){
-//     localStorage.clear();
-//     getData("Denver");
-// };
+function init(){
+    localStorage.clear();
+    getData("Denver");
+};
 
-// init();
+init();
 
-// $('#search-button').on('click', function (event) {
-//     event.preventDefault();
-//     const city = cityName.value;
-//     console.log(city);
-//     if (city) {
-//       getApiData(city);
-//     }
-//   });
+$('#search-button').on('click', function (event) {
+    event.preventDefault();
+    const city = locationName.value;
+    console.log(city);
+    if (city) {
+      getData(city);
+    }
+  });
   
-//   $('#search-city').on('click', function (event) {
-//     event.preventDefault();
-//     const tempElement = event.target;
-//     const city = tempElement.textContent;
-//     getApiData(city);
-//   });
+  $('#search-city').on('click', function (event) {
+    event.preventDefault();
+    const tempElement = event.target;
+    const city = tempElement.textContent;
+    getData(city);
+  });
 
 
 
