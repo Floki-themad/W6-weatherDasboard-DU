70724c423210df7e3f5bf8c9d2bf485043d3c5fd
Floki <ypr_tylerkeuler@aol.com>
2022-06-27 13:05:15 -0400

    added logic to grab api data by city then logic to convert it to lat/lon

--- develop/js/script.js
+++ develop/js/script.js
@@ -5,7 +5,37 @@ const searchLocation = document.getElementById("search-city");
 const cityArr = [];
 
 // get api data on city 
+function getData(inputCity){
+    const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${inputCity}&limit=1&appid=${apiKey}`;
+    fetch(apiUrl)
+    .then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                console.log(data);
+                if(data.length > 0){
+                    const foundLocation = data[0].name;
+                    if(foundLocation.toLowerCase() === inputCity.toLowerCase()){
+                        const lat = data[0].lat;
+                        const lon = data[0].lon;
+                        const newApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${myApiKey}&units=imperial`;
 
+                        fetch(newApiUrl)
+                        .then(function(response){
+                            return response.json();
+                        });
+                        .then(function(data){
+                            console.log(data);
+                            $("#current-city").html(foundLocation);
+                            $("#current-date").html("(Today: " + today.format("l") + ")");
+                            $("#current-icon").attr("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2.png");
+                            $("#current-temp").html
+                        })
+                    }
+                }
+            })
+        }
+    })
+}
 // convert api data to lat/lon
 
 
