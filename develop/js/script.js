2b9b4333056395c3c9ea2fbabe44f5d0f41e9594
Floki <ypr_tylerkeuler@aol.com>
2022-06-27 12:52:55 -0400

    setting globar vars for later use and some sudo coding

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,4 +1,13 @@
 var apiKey = 'c7dc3dc5930fbf4151b143bc06889fef'
+let today = moment();
+const location = document.getElementById("city-name");
+const searchLocation = document.getElementById("search-city");
+const cityArr = [];
+
+// get api data on city 
+
+// convert api data to lat/lon
+
 
 
 
